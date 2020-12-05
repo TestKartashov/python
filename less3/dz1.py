1283f81b52fff187049ffe0a3688da98516b4820
TestPyt <kalex113@yandex.ru>
2020-12-05 19:03:27 +0300

    DZ1

--- less3/dz1.py
+++ less3/dz1.py
@@ -1,1 +1,16 @@
-# 1
+# Реализовать функцию, принимающую два числа (позиционные аргументы) и
+# выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть
+# обработку ситуации деления на ноль.
+
+def del_num(var_1, var_2):
+    try:
+        return var_1 / var_2, ''
+    except ZeroDivisionError as err:
+        return None, err
+
+
+inp_1 = int(input("Enter number One: "))
+inp_2 = int(input("Enter number Two: "))
+
+print(del_num(inp_1, inp_2))
