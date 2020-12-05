cc51b97a671d6deabf5094f20e88fb256009910d
TestPyt <kalex113@yandex.ru>
2020-12-06 00:54:10 +0300

    DZ6

--- less3/dz3.py
+++ less3/dz3.py
@@ -3,27 +3,9 @@
 # возвращает сумму наибольших двух аргументов.
 
 def my_max(var_1, var_2, var_3):
-    ret_1 = var_1
-    ret_2 = var_2
+    l = [var_1, var_2, var_3]
+    l.sort(key=lambda t: -t)
+    return l[0] + l[1]
 
-    if ret_1 < var_3:
-        ret_1 = var_3
-        if ret_2 < var_1:
-            ret_2 = var_1
-    else:
-        if ret_2 < var_3:
-            ret_2 = var_3
-
-    return ret_1 + ret_2
 
 print(my_max(1, 8, 2))
-
-print(my_max(1, 2, 8))
-
-print(my_max(8, 1, 2))
-
-print(my_max(8, 2, 1))
-
-print(my_max(2, 1, 8))
-
-print(my_max(2, 8, 1))
