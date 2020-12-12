1a2dc07617c00d029f83c955ff12612b246b3fc2
TestPyt <kalex113@yandex.ru>
2020-12-12 12:24:46 +0300

    Dz2

---
+++ less5/dz2.py
@@ -0,0 +1,16 @@
+# Создать текстовый файл (не программно),
+# сохранить в нем несколько строк, выполнить подсчет количества строк, количества слов в каждой строке.
+
+
+my_f = open("dz2.txt", "r")
+cnt = 0
+for line in my_f:
+    cnt += 1
+    print("-----------------------------------------")
+
+    print("Count row", cnt)
+    strLs = line.split(" ")
+    strLs = [s for s in strLs if s != "\n"]
+    print("Count слов", len(strLs))
+
+my_f.close()
