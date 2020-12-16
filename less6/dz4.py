c55260135f318ae5f6bcd4836f781571a99d172c
TestPyt <kalex113@yandex.ru>
2020-12-16 22:44:29 +0300

    dz4

--- less6/dz4.py
+++ less6/dz4.py
@@ -13,28 +13,36 @@ class Car:
     speed = float
     color = str
     name = str
-    is_police = bool
+    is_police = False
+    msg = ""
+
+
+
 
     def go(self):
-        print(f"{self.name} Go")
+        self.msg = "Auto"
+        if self.is_police:
+            self.msg = "Auto Police"
+        print(f"{self.name} {self.msg} Go")
 
     def stop(self):
-        print(f"{self.name} Stop")
+        print(f"{self.name} {self.msg} Stop")
 
     def turn(self, direction):
-        print(f"{self.name} turn {direction}")
+        print(f"{self.name} {self.msg} turn {direction}")
 
     def show_speed(self):
-        print(self.speed)
+        print(f"{self.msg} Speed {self.speed}")
 
 
 class TownCar(Car):
+    _stop_speed = 60
 
     def show_speed(self):
-        if self.speed > 60:
-            print(f"{self.name} Alarm Speed > 60: {self.speed}")
+        if self.speed > self._stop_speed:
+            print(f"{self.name} {self.msg} Alarm Speed > {self._stop_speed}: {self.speed}")
         else:
-            print(f"{self.name} Speed > 60: {self.speed}")
+            print(f"{self.name} {self.msg} Speed > 60: {self.speed}")
 
 
 class SportCar(Car):
@@ -42,17 +50,21 @@ class SportCar(Car):
 
 
 class WorkCar(Car):
+    _stop_speed = 40
 
     def show_speed(self):
-        if self.speed > 40:
-            print(f"{self.name} Alarm Speed > 40: {self.speed}")
+        if self.speed > self._stop_speed:
+            print(f"{self.name} {self.msg} Alarm Speed > {self._stop_speed}: {self.speed}")
         else:
-            print(f"{self.name} Speed: {self.speed}")
+            print(f"{self.name} {self.msg} Speed: {self.speed}")
 
 
 class PoliceCar(Car):
-    pass
 
+    def __init__(self):
+        self.is_police = True
+
+print('----------------')
 TownCar = TownCar()
 TownCar.speed = 100
 TownCar.name = "TownCar"
@@ -62,7 +74,7 @@ for i in range(10):
     TownCar.speed -= i * 2
     TownCar.show_speed()
 TownCar.stop()
-
+print('----------------')
 WorkCar = WorkCar()
 WorkCar.speed = 50
 WorkCar.name = "WorkCar"
@@ -73,3 +85,25 @@ for i in range(5):
     WorkCar.show_speed()
 
 WorkCar.stop()
+print('----------------')
+PoliceCar = PoliceCar()
+PoliceCar.speed = 150
+PoliceCar.name = "PoliceCar"
+PoliceCar.color = "red"
+PoliceCar.go()
+for i in range(5):
+    PoliceCar.speed -= i * 2
+    PoliceCar.show_speed()
+
+PoliceCar.stop()
+print('----------------')
+SportCar = SportCar()
+SportCar.speed = 150
+SportCar.name = "SportCar"
+SportCar.color = "red"
+SportCar.go()
+for i in range(5):
+    SportCar.speed -= i * 2
+    SportCar.show_speed()
+
+SportCar.stop()
