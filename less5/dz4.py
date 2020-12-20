1735929590a9f3087e5ea4e0310e74d9c1a3954b
TestPyt <kalex113@yandex.ru>
2020-12-20 15:21:56 +0300

    Исправил ИФ

--- less5/dz4.py
+++ less5/dz4.py
@@ -7,19 +7,13 @@
 # считывающую построчно данные. При этом английские числительные должны
 # заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
 
-
+temp_dict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
 with open("dz4.txt", "r", encoding="UTF-8") as f:
     ls = f.readlines()
     lst = []
     for l in ls:
         ls = l.split(" ")
-        if ls[0] == "One":
-            lst.append(f"Один {ls[1]} {ls[2]}")
-        elif ls[0] == "Two":
-            lst.append(f"Два {ls[1]} {ls[2]}")
-        elif ls[0] == "Three":
-            lst.append(f"Три {ls[1]} {ls[2]}")
-        elif ls[0] == "Four":
-            lst.append(f"Четыре {ls[1]} {ls[2]}")
+        lst.append(f"{temp_dict[ls[0]]} {ls[1]} {ls[2]}")
+
     with open("dz41.txt", "w", encoding="UTF=8") as fw:
         fw.writelines(lst)
