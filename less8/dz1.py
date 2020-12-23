e6592e04ffa85fdeab87cf80cf7b8bfb51727620
TestPyt <kalex113@yandex.ru>
2020-12-23 22:45:29 +0300

    dz2

--- less8/dz1.py
+++ less8/dz1.py
@@ -5,30 +5,36 @@
 # Второй, с декоратором @staticmethod, должен проводить валидацию
 # числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 import datetime
+import re
 
 
 class Data:
     @classmethod
     def ret_number(cls, stroka):
+
+        if not bool(re.match('^[1234567890-]+$', stroka)):
+            raise Exception("Содержит символы")
         dat = stroka.split("-")
+
         return [int(dat[0]), int(dat[1]), int(dat[2])]
 
     @staticmethod
     def valid_num(stroka):
-
         try:
+
             dat = Data.ret_number(stroka)
             t = datetime.date(dat[2], dat[1], dat[0])
         except Exception as e:
             print(e)
             return False
         else:
-
             return True
 
 
 print(Data.ret_number("01-12-2020"))
 
-print(Data.valid_num("01-12-2020"))
+print(Data.valid_num("01-1d2-2020"))
 
 print(Data.valid_num("01-122-2020"))
+
+print(bool(re.match('^[1234567890-]+$', "01-12-2020")))
